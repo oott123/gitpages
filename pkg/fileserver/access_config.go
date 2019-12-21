d99fcee6ccccc04690f1a789c4c76213f55fefe6
oott123 <git@public.oott123.com>
2019-12-21 23:48:24 +0800

    feat: support 404

--- pkg/fileserver/access_config.go
+++ pkg/fileserver/access_config.go
@@ -8,14 +8,19 @@ type AccessConfig struct {
 	// when CORSOrigin is set, only if the request contains Origin header which matches one of the list will get ACAO header.
 	// if CORSOrigin is empty list, ACAO: * will be set.
 	CORSOrigins []string
+	// if AllowDotFiles is true, dot files (files started with `.`) will allowed to access by user.
+	AllowDotFiles bool
+	// if AllowListDirectory is true, access directory which don't contains index files will result a simple list
+	AllowListDirectory bool
+	// if NotFoundErrorPage is defined, 404 will be sent to this file (relative to site root); else will be sent to 404.html
+	NotFoundErrorPage string
 	// Don't use for now
 	ContentType string
-	// if AllowDotFiles is true, dot files (files started with `.`) will allowed to access by user.
-	AllowDotFiles      bool
-	AllowListDirectory bool
-	HotlinkProtection  bool
-	HotlinkOrigins     []string
-	NotFoundErrorPage  string
+	// Don't use for now
+	HotlinkProtection bool
+	// Don't use for now
+	HotlinkOrigins []string
+	// Don't use for now
 	ForbiddenErrorPage string
 }
 
--- pkg/fileserver/serve_http.go
+++ pkg/fileserver/serve_http.go
@@ -1,9 +1,12 @@
 package fileserver
 
 import (
+	"io"
+	"mime"
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 )
 
@@ -22,20 +25,24 @@ func (f *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		varyHeader = append(varyHeader, v)
 	}
 
-	if !f.accessConfig.AllowDotFiles {
-		baseName := path.Base(filename)
-		if strings.Index(baseName, ".") == 0 {
-			// is dotfile
-			allowAccess = false
+	if !accessConfig.AllowDotFiles {
+		pieces := strings.Split(filename, string(os.PathSeparator))
+		for _, p := range pieces {
+			if strings.Index(p, ".") == 0 {
+				// is dotfile
+				f.log.Debugf("%s is dotfile, not allowed", filename)
+				allowAccess = false
+				break
+			}
 		}
 	}
 
-	if f.accessConfig.AllowCORS {
-		if len(f.accessConfig.CORSOrigins) == 0 {
+	if accessConfig.AllowCORS {
+		if len(accessConfig.CORSOrigins) == 0 {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 		} else {
 			varyHeader = append(varyHeader, "Origin")
-			for _, o := range f.accessConfig.CORSOrigins {
+			for _, o := range accessConfig.CORSOrigins {
 				if o == r.Header.Get("Origin") {
 					w.Header().Set("Access-Control-Allow-Origin", o)
 					break
@@ -49,18 +56,55 @@ func (f *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if filename == "" || !allowAccess {
-		// TODO: 403
+		http.Error(w, "access denied", 403)
+		return
 	} else if os.IsNotExist(statErr) {
-		// TODO: 404
+		// 404 fall through
 	} else if stat.IsDir() {
 		// directory
 		indexFilename := path.Join(filename, "index.html")
 		if _, err := os.Stat(indexFilename); os.IsNotExist(err) {
-			// TODO: directory listing
+			if accessConfig.AllowListDirectory {
+				http.ServeFile(w, r, filename)
+			} else {
+				http.Error(w, "directory listing is not allowed", 403)
+			}
 		} else {
 			http.ServeFile(w, r, filename)
 		}
+		return
 	} else {
 		http.ServeFile(w, r, filename)
+		return
+	}
+
+	// 404 here
+	if accessConfig.NotFoundErrorPage != "" {
+		filename = f.Resolve(accessConfig.NotFoundErrorPage)
+	} else {
+		filename = f.Resolve("/404.html")
+	}
+
+	if filename != "" {
+		ext := filepath.Ext(filename)
+		mType := mime.TypeByExtension(ext)
+		if mType == "" {
+			mType = "text/plain"
+		}
+		w.Header().Set("Content-Type", mType)
+		w.Header().Del("Date")
+		w.Header().Del("Last-Modified")
+		w.Header().Set("Cache-Control", "no-cache")
+
+		w.WriteHeader(404)
+		file, err := os.Open(filename)
+		defer file.Close()
+		if err != nil {
+			f.log.Errorf("error while opening 404 file: %s", err)
+			http.Error(w, "404 Not found", 404)
+			return
+		}
+		_, _ = io.Copy(w, file)
+		return
 	}
 }
