2782d402312f816cdb9d480c5b32ed14263696a1
oott123 <git@public.oott123.com>
2019-12-22 00:23:34 +0800

    feat: add js mime

--- internal/app/router/init.go
+++ internal/app/router/init.go
@@ -3,10 +3,15 @@ package router
 import (
 	"github.com/oott123/gitpages/pkg/logger"
 	"go.uber.org/zap"
+	"mime"
 )
 
 var log *zap.SugaredLogger
 
 func init() {
 	log = logger.New()
+	err := mime.AddExtensionType(".js", "application/javascript")
+	if err != nil {
+		panic(err)
+	}
 }
