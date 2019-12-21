d3ac7efc7b564faec77e3ab82f36f3f0231e0ecb
oott123 <git@public.oott123.com>
2019-12-21 23:11:46 +0800

    feat: cors and basics

--- pkg/fileserver/access_config.go
+++ pkg/fileserver/access_config.go
@@ -3,9 +3,14 @@ package fileserver
 import "net/http"
 
 type AccessConfig struct {
-	AllowCORS          bool
-	CORSOrigins        []string
-	ContentType        string
+	// if AllowCORS is true, a Access-Control-Allow-Origin header will be set. see CORSOrigins for details.
+	AllowCORS bool
+	// when CORSOrigin is set, only if the request contains Origin header which matches one of the list will get ACAO header.
+	// if CORSOrigin is empty list, ACAO: * will be set.
+	CORSOrigins []string
+	// Don't use for now
+	ContentType string
+	// if AllowDotFiles is true, dot files (files started with `.`) will allowed to access by user.
 	AllowDotFiles      bool
 	AllowListDirectory bool
 	HotlinkProtection  bool
--- pkg/fileserver/serve_http.go
+++ pkg/fileserver/serve_http.go
@@ -3,6 +3,8 @@ package fileserver
 import (
 	"net/http"
 	"os"
+	"path"
+	"strings"
 )
 
 var _ http.Handler = (*FileServer)(nil)
@@ -13,12 +15,51 @@ func (f *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	accessConfig := f.AccessConfig(r)
 	f.log.Debugf("parsed access config: %#v", accessConfig)
 
-	if filename == "" {
+	allowAccess := true
+	varyHeader := make([]string, 0)
+
+	if v := w.Header().Get("Vary"); v != "" {
+		varyHeader = append(varyHeader, v)
+	}
+
+	if !f.accessConfig.AllowDotFiles {
+		baseName := path.Base(filename)
+		if strings.Index(baseName, ".") == 0 {
+			// is dotfile
+			allowAccess = false
+		}
+	}
+
+	if f.accessConfig.AllowCORS {
+		if len(f.accessConfig.CORSOrigins) == 0 {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		} else {
+			varyHeader = append(varyHeader, "Origin")
+			for _, o := range f.accessConfig.CORSOrigins {
+				if o == r.Header.Get("Origin") {
+					w.Header().Set("Access-Control-Allow-Origin", o)
+					break
+				}
+			}
+		}
+	}
+
+	if len(varyHeader) > 0 {
+		w.Header().Set("Vary", strings.Join(varyHeader, ", "))
+	}
+
+	if filename == "" || !allowAccess {
 		// TODO: 403
 	} else if os.IsNotExist(statErr) {
 		// TODO: 404
 	} else if stat.IsDir() {
-		// TODO: directory
+		// directory
+		indexFilename := path.Join(filename, "index.html")
+		if _, err := os.Stat(indexFilename); os.IsNotExist(err) {
+			// TODO: directory listing
+		} else {
+			http.ServeFile(w, r, filename)
+		}
 	} else {
 		http.ServeFile(w, r, filename)
 	}
